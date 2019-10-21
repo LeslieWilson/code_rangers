58e7bde3cf7e086ab0a2b556a450ea1700356eaf
Nick Jamison <nickmcjamison@gmail.com>
2019-10-21 13:59:09 -0400

    complete add new review form with tests

--- spec/controllers/api/vi/reviews_controller_spec.rb
+++ spec/controllers/api/vi/reviews_controller_spec.rb
@@ -1,34 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::ReviewsController, type: :controller do
-  let!(:first_park) {Park.create(
-    name: "leslie",
-    location: "montana",
-    description: "old faithful",
-    image: "hello picture",
-  )}
+  let!(:park5) {
+    FactoryBot.create(:park)
+    }
 
-  let!(:first_review) {Review.create(
-    rating: "5",
-    review_body: "It was awesome!"
-  )}
+  describe "POST#create" do
+    it "should should sucessfully post a review when all fields are filled in" do
+      review2 = { review: {
+        rating: "5",
+        review_body: "It was awesome!",
+        park_id: park5.id
+      }}
 
-describe "POST#create" do
-  it "should should sucessfully post a review when all fields are filled in" do
-    sample_review = { review: {
-      rating: "5",
-      review_body: "It was awesome!"
-    }}
+      post :create, :params => {park_id: park5.id, review: review2}, format: :json
+      returned_json = JSON.parse(response.body)
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq("application/json")
 
-    post :create, :params => sample_review, format: :json
-    returned_json = JSON.parse(response.body)
-    expect(response.status).to eq 200
-    expect(response.content_type).to eq("application/json")
-
-    expect(returned_json).to be_kind_of(Hash)
-    expect(returned_json).to_not be_kind_of(Array)
-    expect(returned_json["rating"]).to eq sample_review[:rating]
-    expect(returned_json["review_body"]).to eq sample_review[:review_body]
+      expect(returned_json).to be_kind_of(Hash)
+      expect(returned_json).to_not be_kind_of(Array)
+      expect(returned_json["rating"]).to eq review2[:rating]
+      expect(returned_json["review_body"]).to eq review2[:review_body]
+    end
   end
-end
 end
