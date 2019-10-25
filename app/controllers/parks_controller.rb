0c156052a18006cbc4f468dfda75bcab585d2830
Daniel <hanakoscar@outlook.com>
2019-10-25 11:25:31 -0400

    fixed broken dependency

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,6 +1,5 @@
 class ParksController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
-  before_action :set_s3_direct_post, only: [:edit, :update]
 
   def new
   end
@@ -25,7 +24,4 @@ class ParksController < ApplicationController
     params.require(:park).permit(:name, :location, :description, :image)
   end
 
-  def set_s3_direct_post
-    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
-  end
 end
