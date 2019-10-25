285aacf8c29d847eddc292a7191c46de4e855501
amymliebs <amymliebs@gmail.com>
2019-10-25 12:04:00 -0400

    saved changes

--- app/javascript/react/components/ReviewTile.js
+++ app/javascript/react/components/ReviewTile.js
@@ -29,15 +29,10 @@ const ReviewTile = (props) => {
   return(
     <div>
       <div className="single-review">
-<<<<<<< HEAD
-        <div id="rating"><b>Rating:  </b>{props.rating}/5</div>
-        <div id="review-body"><b>Review:  </b>{props.reviewBody}</div>
-=======
         <div id="rating" className="rating-stars">
           <b>Rating:</b> {stars}
         </div>
         <p id="review-body"><b>Review:  </b>{props.reviewBody}</p>
->>>>>>> 7375d47438e77254847f2021cefd367d06d4df72
         <button className={`${visibility} button delete-review-button`} onClick={handleDeleteReviewClick}>DELETE MY REVIEW</button>
       </div>
       <hr className="divider"/>
